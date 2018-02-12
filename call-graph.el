d33374e03b1f73a09c3608008151c64d76f27c01
Huming Chen <huming.chen@ericsson.com>
2018-02-12 12:57:11 +0100

    Add major change to tackle the same method issue.
    It's a draft now, need more testing.
    
    Change-Id: I576d83d1a58888a14142f9f6f9217cf6dc7ee32e

--- call-graph.el
+++ call-graph.el
@@ -92,10 +92,132 @@
 ;; Helpers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(cl-defstruct (call-graph
+               (:constructor call-graph--make)
+               (:conc-name call-graph--))
+  (depth 0)  ; depth of call graph
+  (hierarchy (hierarchy-new)) ; hierarchy to display call-graph
+  (callers (make-hash-table :test 'equal)) ; map func to its callers
+  (locations (make-hash-table :test 'equal))) ; map func<-caller to its locations
+
+(defun call-graph-new ()
+  "Create a call-graph and return it."
+  (call-graph--make))
+
+(defun call-graph--add-callers (call-graph func callers)
+  "In CALL-GRAPH, for FUNC, add all its CALLERS."
+  (let ((its-callers (map-elt (call-graph--callers call-graph) func)))
+    (unless its-callers
+      (seq-doseq (caller callers)
+        (let* ((full-caller (symbol-name (car caller))) ; class::method
+               (caller-split (split-string full-caller "::"))
+               (short-caller (intern (seq-elt caller-split (1- (seq-length caller-split))))) ; method only
+               (func-caller-key (intern (concat (symbol-name func) " <- " (symbol-name short-caller))))) ; "callee <- caller" as key
+
+          ;; populate location data
+          (push caller (map-elt (call-graph--locations call-graph) func-caller-key))
+
+          ;; populate caller data
+          (push short-caller (map-elt (call-graph--callers call-graph) func)))))))
+
+(defun call-graph--find-caller-map2 (call-graph func depth)
+  "In CALL-GRAPH, given a FUNC, return its caller-map.
+DEPTH is the depth of caller-map, SEEN-CALLERS prevent infinite loop."
+  (when-let ((next-depth (and (> depth 0) (1- depth))))
+    (let ((caller-pairs (list)))
+
+      ;; callers not found.
+      (unless (map-elt (call-graph--callers call-graph) func)
+        (seq-doseq (reference (call-graph--find-references func))
+          (when-let ((caller-pair
+                      (and reference (call-graph--find-caller2 reference))))
+            (message (format "Search returns: %s" (symbol-name (car caller-pair))))
+            (push caller-pair caller-pairs)))
+        (call-graph--add-callers call-graph func caller-pairs))
+
+      (seq-doseq (caller (map-elt (call-graph--callers call-graph) func))
+        (message "caller is %s" (symbol-name caller)))
+
+      ;; recursively find callers.
+      (seq-doseq (caller (map-elt (call-graph--callers call-graph) func))
+        (call-graph--find-caller-map2 call-graph caller next-depth)))))
+
+(defun call-graph--display-caller-map2 (call-graph func &optional actual-depth)
+  "In CALL-GRAPH, given FUNC, display its CALLER-MAP, calculate ACTUAL-DEPTH."
+  (when-let ((hierarchy (call-graph--hierarchy call-graph))
+             (callers (call-graph--callers call-graph))
+             (locations (call-graph--locations call-graph))
+             (actual-depth (or actual-depth 1))
+             (next-depth (1+ actual-depth)))
+
+    ;; populate hierarchy data.
+    (seq-doseq (caller callers)
+      (let* ((func-caller-key (intern (concat (symbol-name func) "<-" (symbol-name caller))))
+             (caller-locations (map-elt locations func-caller-key)))
+        (seq-doseq (caller-location caller-locations)
+          (let ((full-caller (car caller-location))
+                (location (cdr caller-location)))
+            (put full-caller 'caller-location location)
+            (hierarchy-add-tree hierarchy full-caller (lambda (item) (when (eq item full-caller) func)))
+            (message "insert child %s under parent %s" (symbol-name full-caller) (symbol-name func))))))
+
+    ;; calculate depth.
+    (when (> actual-depth (call-graph--depth call-graph))
+      (setq (call-graph--depth call-graph) actual-depth))
+
+    ;; recursively populate callers.
+    (seq-doseq (caller callers)
+      (call-graph--display-caller-map2 call-graph caller next-depth))))
+
+(defun call-graph--hierarchy-display2 (call-graph)
+  "Display CALL-GRAPH."
+  (let ((switch-buffer (not (eq major-mode 'call-graph-mode)))
+        hierarchy-buffer)
+    (setq hierarchy-buffer
+          (hierarchy-tree-display
+           (call-graph--hierarchy call-graph)
+           (lambda (tree-item _)
+             (let* ((location (get tree-item 'caller-location))
+                    (caller (symbol-name tree-item)))
+               ;; use propertize to avoid this error => Attempt to modify read-only object
+               ;; @see https://stackoverflow.com/questions/24565068/emacs-text-is-read-only
+               (insert (propertize caller 'caller-location location))))
+           (call-graph--get-buffer)))
+    (when switch-buffer
+      (switch-to-buffer-other-window hierarchy-buffer))
+    (call-graph-mode)
+    (call-graph-widget-expand-all)))
+
+(defun call-graph--display2 (call-graph func)
+  "Prepare data and display CALL-GRAPH for FUNC."
+  (call-graph--display-caller-map2 call-graph func)
+  (call-graph--hierarchy-display2 call-graph))
+
 (defun call-graph--get-buffer ()
   "Generate ‘*call-graph*’ buffer."
   (let ((buffer-name "*call-graph*"))
     (get-buffer-create buffer-name)))
+
+(defun call-graph--find-caller2 (reference)
+  "Given a REFERENCE, return the caller as (caller . location)."
+  (when-let ((tmp-val (split-string reference ":"))
+             (file-name (seq-elt tmp-val 0))
+             (line-nb-str (seq-elt tmp-val 1))
+             (line-nb (string-to-number line-nb-str))
+             (is-valid-file (file-exists-p file-name))
+             (is-valid-nb (integerp line-nb)))
+    (let ((location (concat file-name ":" line-nb-str))
+          caller)
+      (with-temp-buffer
+        (insert-file-contents-literally file-name)
+        ;; TODO: leave only hooks on which 'which-function-mode depends
+        ;; (set (make-local-variable 'c++-mode-hook) nil)
+        (c++-mode)
+        (which-function-mode t)
+        (forward-line line-nb)
+        (setq caller (which-function)))
+      (when caller
+        (cons (intern caller) location)))))
 
 (defun call-graph--find-caller (reference)
   "Given a REFERENCE, return the caller as (caller . location)."
@@ -220,6 +342,14 @@ DEPTH is the depth of caller-map, SEEN-CALLERS prevent infinite loop."
       ;; return top-level caller-map.
       caller-map)))
 
+(defun call-graph--create2 (func depth)
+  "Generate `call-graph' for FUNC.
+DEPTH is the depth of caller-map."
+  (when-let ((call-graph (call-graph-new)))
+    (call-graph--find-caller-map2 call-graph func depth)
+    (call-graph--display2 call-graph func)
+    (setq call-graph--hierarchy call-graph)))
+
 (defun call-graph--create (func depth)
   "Generate `call-graph' for FUNC.
 DEPTH is the depth of caller-map."
@@ -240,8 +370,8 @@ DEPTH is the depth of caller-map."
                (depth (or depth call-graph-initial-max-depth)))
       (setq call-graph--internal-cache nil)
       (if (> depth call-graph-initial-max-depth)
-          (call-graph--create func depth)
-        (call-graph--create func call-graph-initial-max-depth)))))
+          (call-graph--create2 func depth)
+        (call-graph--create2 func call-graph-initial-max-depth)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Call-Graph operation
