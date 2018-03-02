9413fd181e46482d95ed892b45386f80796d2a8a
陈虎明 <chenhuming@gmail.com>
2018-03-02 13:24:46 +0800

    Add cl-lib dependency.

--- call-graph.el
+++ call-graph.el
@@ -8,7 +8,7 @@
 ;; Version: 0.0.8
 ;; Keywords: programming, convenience
 ;; Created: 2018-01-07
-;; Package-Requires: ((emacs "25.1") (hierarchy "0.7.0") (tree-mode "1.0.0") (ivy "0.10.0"))
+;; Package-Requires: ((emacs "25.1") (cl-lib "1.0") (hierarchy "0.7.0") (tree-mode "1.0.0") (ivy "0.10.0"))
 
 ;; This file is not part of GNU Emacs.
 
