1ba83f20e56cfe77f6165df4ffde1d152647ec3b
陈虎明 <chenhuming@gmail.com>
2018-05-09 21:35:19 +0800

    Fix cg--get-path-to-global

--- call-graph.el
+++ call-graph.el
@@ -175,9 +175,12 @@ e.g: class::method => method."
 
 (defun cg--get-path-to-global ()
   "Return path to program GNU GLOBAL."
-  (let ((path (if cg-path-to-global (expand-file-name "global" cg-path-to-global) "global")))
-    (if (file-exists-p path) path
-      (error "Failed to find \"GNU GLOBAL\" in path: %s" path))))
+  (let ((absolute-path
+         (or (executable-find "global")
+             (expand-file-name "global" cg-path-to-global))))
+    (unless (file-exists-p absolute-path)
+      (error "Failed to find \"GNU GLOBAL\" in path: %s" absolute-path))
+    absolute-path))
 
 (defun cg--dwim-at-point ()
   "If there's an active selection, return that.
