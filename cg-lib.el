a1eb6b22c447c74d37a85b3acc9f6687e7640e98
陈虎明 <chenhuming@gmail.com>
2019-01-02 16:19:14 +0800

    Add function cg--customize-c++-generic-expression.

--- cg-lib.el
+++ cg-lib.el
@@ -32,15 +32,6 @@
 (require 'which-func)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Customizable
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defcustom cg-display-func-args nil
-  "Non-nil means display function together with its args in `call-graph'."
-  :type 'boolean
-  :group 'call-graph)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Definition
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -58,6 +49,12 @@
    "\\(?1:[" c-alpha "_][" c-alnum "_:<>~]*\\)" ;; match function name
    "\\([ \t\n]\\|\\\\\n\\)*(") ;; match left-parens
   "Regexp to match function til its left parens.")
+
+(defconst cg--orgin-cc-menus-association (assoc 'cc-menus after-load-alist)
+  "Save currunt association value for entry `cc-menus' in `after-load-alist'.")
+
+(defconst cg--orgin-general-function-name-regexp (nth 2 cc-imenu-c++-generic-expression)
+  "Save current generic fucntion name regexp.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helpers
@@ -98,34 +95,35 @@ White space here is any of: space, tab, Emacs newline (line feed, ASCII 10)."
 ;; Routines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; enable imenu to display both function name and its arg-list
-(defun cg--imenu-show-func-args()
-  "For current buffer, show function name and its arg-list."
-  (setq-local c++-mode-hook nil)
-  (c++-mode)
-  (when cg-display-func-args
-    (make-local-variable 'cc-imenu-c-generic-expression)
-    (make-local-variable 'cc-imenu-c++-generic-expression)
-    (setf (nth 2 cc-imenu-c++-generic-expression)
-          ;; General function name regexp
-          `(nil
-            ,(concat
-              "^\\<"                                 ; line MUST start with word char
-              ;; \n added to prevent overflow in regexp matcher.
-              ;; https://lists.gnu.org/r/emacs-pretest-bug/2007-02/msg00021.html
-              "[^()\n]*"                             ; no parentheses before
-              "[^" c-alnum "_:<>~]"                  ; match any non-identifier char
-              "\\(?2:\\(?1:[" c-alpha "_][" c-alnum "_:<>~]*\\)" ; 2ND-GROUP MATCH FUNCTION AND ITS ARGS WHILE 1ST-GROUP MATCH FUNCTION NAME
-              "\\([ \t\n]\\|\\\\\n\\)*("            ; see above, BUT the arg list
-              "\\([ \t\n]\\|\\\\\n\\)*"             ; must not start
-              "\\([^ \t\n(*]"                       ; with an asterisk or parentheses
-              "[^()]*\\(([^()]*)[^()]*\\)*"         ; Maybe function pointer arguments
-              "\\)?)\\)"                            ; END OF 2ND-GROUP
-              "\\([ \t\n]\\|\\\\\n\\)*[^ \t\n;(]"
-              ) 2)                                  ; USE 2ND-GROUP AS IMENU ITEM
-          cc-imenu-c-generic-expression cc-imenu-c++-generic-expression))
-  (setq-local which-func-cleanup-function nil)
-  (which-function-mode t))
+(defun cg--customize-c++-generic-expression (toggle)
+  "When TOGGLE is non-nil, customiz c++-generic-expression to support function args.
+else, retore original c++-generic-expression."
+  (if toggle
+      (with-eval-after-load 'cc-menus
+        ;; enable imenu to display both function name and its arg-list
+        (setf (nth 2 cc-imenu-c++-generic-expression)
+              ;; General function name regexp
+              `(nil
+                ,(concat
+                  "^\\<"                                 ; line MUST start with word char
+                  ;; \n added to prevent overflow in regexp matcher.
+                  ;; https://lists.gnu.org/r/emacs-pretest-bug/2007-02/msg00021.html
+                  "[^()\n]*"                             ; no parentheses before
+                  "[^" c-alnum "_:<>~]"                  ; match any non-identifier char
+                  "\\(?2:\\(?1:[" c-alpha "_][" c-alnum "_:<>~]*\\)" ; 2ND-GROUP MATCH FUNCTION AND ITS ARGS WHILE 1ST-GROUP MATCH FUNCTION NAME
+                  "\\([ \t\n]\\|\\\\\n\\)*("            ; see above, BUT the arg list
+                  "\\([ \t\n]\\|\\\\\n\\)*"             ; must not start
+                  "\\([^ \t\n(*]"                       ; with an asterisk or parentheses
+                  "[^()]*\\(([^()]*)[^()]*\\)*"         ; Maybe function pointer arguments
+                  "\\)?)\\)"                            ; END OF 2ND-GROUP
+                  "\\([ \t\n]\\|\\\\\n\\)*[^ \t\n;(]"
+                  ) 2)                                  ; USE 2ND-GROUP AS IMENU ITEM
+              cc-imenu-c-generic-expression cc-imenu-c++-generic-expression))
+    (progn
+      (setq after-load-alist (assq-delete-all 'cc-menus after-load-alist))
+      (when cg--orgin-cc-menus-association
+        (push cg--orgin-cc-menus-association after-load-alist))
+      (setf (nth 2 cc-imenu-c++-generic-expression) cg--orgin-general-function-name-regexp))))
 
 (defun cg--number-of-args(func-with-args)
   "Count number of C++ function arguments of FUNC-WITH-ARGS."
