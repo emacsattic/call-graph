9d6e9a75630eace3c1cca8b55e16057fc9bf4252
陈虎明 <chenhuming@gmail.com>
2023-01-12 09:21:45 +0800

    Update call-graph.el

--- call-graph.el
+++ call-graph.el
@@ -184,7 +184,7 @@
              (is-valid-file (file-exists-p file-name))
              (is-valid-nb (integerp line-nb)))
     (find-file-read-only-other-window file-name)
-    (with-no-warnings (forward-line (1- line-nb))
+    (with-no-warnings (goto-line line-nb) ;; don't use forward-line, it's not working well here
                       (pulse-momentary-highlight-region (line-beginning-position) (line-end-position)))
     (unless (member
              (buffer-name (window-buffer))
