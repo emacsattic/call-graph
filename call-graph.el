18a96dbabbedcd9e55817af7b6a0f303aea09faa
陈虎明 <chenhuming@gmail.com>
2023-02-20 10:26:28 +0800

    Don't pulse highlight since it will leads to cusor all around the place.

--- call-graph.el
+++ call-graph.el
@@ -188,8 +188,7 @@
              (is-valid-nb (integerp line-nb)))
     (find-file-read-only-other-window file-name)
     (with-no-warnings (goto-char (point-min))
-                      (forward-line (1- line-nb))
-                      (pulse-momentary-highlight-region (line-beginning-position) (line-end-position)))
+                      (forward-line (1- line-nb)))
     (unless (member
              (buffer-name (window-buffer))
              (cl-loop for buffer in cg--previous-buffers
